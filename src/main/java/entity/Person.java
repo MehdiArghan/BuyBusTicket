957ac9a4aa2b93391b247e92b55e110cb6001900
mehdi <mehdiarghan@gmail.com.git/>
2024-02-25 13:20:16 +0330

    added Ticket class

--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -1,11 +1,14 @@
 package entity;
 
 import base.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
-import java.util.List;
 
 @Setter
 @Getter
@@ -16,12 +19,22 @@ import java.util.List;
 @Entity
 @Builder
 public class Person extends BaseEntity<Long> {
+    @NotNull(message = "firstName cannot be null")
+    @NotEmpty(message = "firstName cannot be empty")
+    @Column(nullable = false)
     String firstName;
+    @NotNull(message = "lastName cannot be null")
+    @NotEmpty(message = "lastName cannot be empty")
+    @Column(nullable = false)
     String lastName;
     @Enumerated(EnumType.STRING)
     Gender gender;
+    @NotNull(message = "userName cannot be null")
+    @NotEmpty(message = "userName cannot be empty")
+    @Column(nullable = false)
     String userName;
+    @NotNull(message = "password cannot be null")
+    @NotEmpty(message = "password cannot be empty")
+    @Column(nullable = false)
     String password;
-    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
-    List<Ticket> ticketList;
 }
--- src/main/java/entity/Ticket.java
+++ src/main/java/entity/Ticket.java
@@ -1,6 +1,30 @@
 package entity;
 
 import base.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Builder
 public class Ticket extends BaseEntity<Long> {
+    String origin;
+    String destination;
+    @Temporal(TemporalType.DATE)
+    LocalDate departureDate;
+    String departureTime;
+    String travelId;
+    @ManyToOne
+    Person person;
 }
