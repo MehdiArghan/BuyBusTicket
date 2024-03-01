0c2892844352753b72c8320c0972ff099239a422
mehdi <mehdiarghan@gmail.com.git/>
2024-03-01 16:54:20 +0330

    remove restrictions on Person entity

--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -1,12 +1,9 @@
 package entity;
 
 import base.entity.BaseEntity;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -19,22 +16,10 @@ import lombok.experimental.FieldDefaults;
 @Entity
 @Builder
 public class Person extends BaseEntity<Long> {
-    @NotNull(message = "firstName cannot be null")
-    @NotEmpty(message = "firstName cannot be empty")
-    @Column(nullable = false)
     String firstName;
-    @NotNull(message = "lastName cannot be null")
-    @NotEmpty(message = "lastName cannot be empty")
-    @Column(nullable = false)
     String lastName;
     @Enumerated(EnumType.STRING)
     Gender gender;
-    @NotNull(message = "userName cannot be null")
-    @NotEmpty(message = "userName cannot be empty")
-    @Column(nullable = false)
     String userName;
-    @NotNull(message = "password cannot be null")
-    @NotEmpty(message = "password cannot be empty")
-    @Column(nullable = false)
     String password;
 }
